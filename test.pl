1aecaf03b638bd2c3f8821ba7621b873c108ceab
j-navesx <j.naves@campus.fct.unl.pt>
2021-05-10 09:55:13 +0100

    stuff

--- test.pl
+++
@@ -1,9 +0,0 @@
-dist(a,b,50).
-dist(a,c,30).
-dist(b,d,60).
-dist(c,b,20).
-dist(c,d,25).
-dist(c,e,40).
-
-distance(X,Y,D) :- dist(X,Y,D).
-distance(X,Y,D) :- dist(X,Z,D1), distance(Z,Y, D2), D is D1 + D2.
