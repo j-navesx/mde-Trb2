f6e65f3ec71dddda2b25596adec1590eb832fffb
j-navesx <j.naves@campus.fct.unl.pt>
2021-05-17 10:30:36 +0100

    created factories and transport memory files

---
+++ facts.pl
@@ -0,0 +1,8 @@
+%fact(node_name, [items_produced])
+
+:- dynamic fact/2.
+
+fact(a, [pneus, pastilhas]).
+fact(b, [parabrisas, janelas]).
+fact(c, [jantesvw, portasvw, capovw, custombodywork]).
+fact(d, [motores1300diesel, motores2000gasolina]).
