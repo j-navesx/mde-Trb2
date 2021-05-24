3fefe4cae344a7285109b798aa0643a9ef0d9b29
g-pombo <45361838+g-pombo@users.noreply.github.com>
2021-05-24 10:01:37 +0100

    Validação correta

--- facts.pl
+++ facts.pl
@@ -6,6 +6,5 @@ fact(c, [jantesvw, portasvw, capovw, custombodywork]).
 fact(d, [motores1300diesel, motores2000gasolina]).
 fact(hallo, [prod1, prod2, prod3]).
 fact(fact1, [prod1, prod2, prod3]).
-fact(hallo, [prod1, prod2, prod3]).
 fact(fact, ['1', '2', '3']).
 
--- transps.pl
+++ transps.pl
@@ -1,6 +1,6 @@
 :- dynamic transp/2.
 
-%transp(transpname, [transp_type, (Km/h)med, Emitions/Km, price/Km]).
-
 transp(transp1, [[ship1, 90, 80, 50]]).
-transp(transp2, [[ship2, 70, 40, 60],[plane1, 250, 60, 110]]).
+transp(transp2, [[ship2, 70, 40, 60], [plane1, 250, 60, 110]]).
+transp(transp3, [[truck1, 90, 50, 30]]).
+
